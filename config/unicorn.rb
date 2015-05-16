0008e9297fb7b9038beaa44c99ad5cd5b6d292af
pjaspers <piet@pjaspers.com>
2015-05-16 20:51:13 +0200

    Disconnect Sequel on fork from unicorn.
    Ran into this on random occasions:
    
        PQconsumeInput() SSL SYSCALL error: EOF detected
    
    which lead me to this:
    
    https://devcenter.heroku.com/articles/postgres-logs-errors#pgerror-ssl-syscall-error-eof-detected
    
    which lead me to this:
    
    https://devcenter.heroku.com/articles/forked-pg-connections
    
    which lead me to this:
    
    http://sequel.jeremyevans.net/rdoc/files/doc/code_order_rdoc.html
    
    which should fix it.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,24 @@
 worker_processes 3
 timeout 30
 preload_app true
+
+before_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  if defined?($db)
+    $db.disconnect
+  end
+end
+
+after_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  end
+
+  # Sequel is smart enough to start a new connection in the child process
+end
